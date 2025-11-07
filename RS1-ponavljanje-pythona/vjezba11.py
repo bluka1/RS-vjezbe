c824d3b22067e8b7f3330f8afa17f647cb37a9a1
Luka Batarelo <luka.batarelo@thoughtcap.com>
2025-11-07 17:45:18 +0100

    main - RS1 - vjezba 11

---
+++ RS1-ponavljanje-pythona/vjezba11.py
@@ -0,0 +1,15 @@
+def grupiraj_po_paritetu(lista):
+  brojevi = {
+    "parni": [],
+    "neparni": []
+  }
+
+  for broj in lista:
+    if broj % 2 == 0:
+      brojevi["parni"].append(broj)
+    else:
+      brojevi["neparni"].append(broj)
+  return brojevi
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(grupiraj_po_paritetu(lista))
