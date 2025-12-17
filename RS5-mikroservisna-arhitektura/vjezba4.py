82d1ccdf02a4312e1c4040f577714e27cdf6e7a9
Luka Batarelo <luka.batarelo@thoughtcap.com>
2025-12-17 18:39:43 +0100

    main - RS5-mikroservisna-arhitektura - vjezba4

--- RS5-mikroservisna-arhitektura/vjezba4.py
+++ RS5-mikroservisna-arhitektura/vjezba4.py
@@ -1,12 +1,9 @@
-# Definirajte aiohttp poslužitelj koji radi na portu 8081 . Poslužitelj mora imati dvije rute: /proizvodi i
-# /proizvodi/{id} . Prva ruta vraća listu proizvoda u JSON formatu, a druga rutu vraća točno jedan proizvod
-# prema ID-u. Ako proizvod s traženim ID-em ne postoji, vratite odgovor s statusom 404 i porukom
-# {'error': 'Proizvod s traženim ID-em ne postoji'} .
-# Proizvode pohranite u listu rječnika:
+# Zadatak 4: Dohvaćanje proizvoda
+# Definirajte aiohttp poslužitelj koji radi na portu 8081. Poslužitelj mora imati dvije rute: /proizvodi i /proizvodi/{id}. 
+# Prva ruta vraća listu proizvoda u JSON formatu, a druga rutu vraća točno jedan proizvod prema ID-u. 
+# Ako proizvod s traženim ID-em ne postoji, vratite odgovor s statusom 404 i porukom {'error': 'Proizvod s traženim ID-em ne postoji'}.
 
-from aiohttp import web
-from aiohttp.web import AppRunner
-import asyncio, aiohttp
+# Proizvode pohranite u listu rječnika:
 
 proizvodi = [
   {"id": 1, "naziv": "Laptop", "cijena": 5000},
@@ -15,21 +12,21 @@ proizvodi = [
   {"id": 4, "naziv": "Monitor", "cijena": 1000},
   {"id": 5, "naziv": "Slušalice", "cijena": 50}
 ]
+# Testirajte poslužitelj na sve slučajeve kroz klijentsku sesiju unutar main korutine iste skripte.
+
+from aiohttp import web
+from aiohttp.web import AppRunner
+import asyncio, aiohttp
 
 async def get_proizvodi(request):
-  return web.json_response(proizvodi)
+  return web.json_response(proizvodi, status=200)
 
 async def get_proizvod_by_id(request):
-  proizvod_id = request.match_info.get('id')
-  if proizvod_id is None:
-    return web.json_response({'error': 'ID nije naveden.'}, status=400)
-  
+  id = request.match_info.get('id')
   for p in proizvodi:
-    if p['id'] == int(proizvod_id):
-      return web.json_response(p, status=200)
-    else:
-      return web.json_response({'error': 'Proizvod s traženim ID-em ne postoji'}, status=404)
-
+    if p['id'] == int(id):
+      return web.json_response(p)
+  return web.json_response({'error': 'Proizvod s traženim ID-em ne postoji'}, status=404)
 
 app = web.Application()
 app.router.add_get('/proizvodi', get_proizvodi)
@@ -38,24 +35,19 @@ app.router.add_get('/proizvodi/{id}', get_proizvod_by_id)
 async def start_server():
   runner = AppRunner(app)
   await runner.setup()
-  site = web.TCPSite(runner, 'localhost', 8080)
+  site = web.TCPSite(runner, 'localhost', 8081)
   await site.start()
-  print("Poslužitelj sluša na http://localhost:8080")
+  print('Server je startao na - http://localhost:8081')
+
 
 async def main():
-  await start_server()
+  asyncio.create_task(start_server())
   async with aiohttp.ClientSession() as session:
-    res = await session.get('http://localhost:8080/proizvodi')
-    proizvodi = await res.text()
-    print(proizvodi)
-    res2 = await session.get('http://localhost:8080/proizvodi/6')
-    proizvod = await res2.text()
-    print(proizvod)
-    res3 = await session.get('http://localhost:8080/proizvodi/')
-    proizvod2 = await res3.text()
-    print(proizvod2)
-    res4 = await session.get('http://localhost:8080/proizvodi/1')
-    proizvod3 = await res4.text()
-    print(proizvod3)
+    proizvodi_res = await session.get('http://localhost:8081/proizvodi')
+    print('proizvodi_res: ', await proizvodi_res.text())
+    proizvod_1_res = await session.get('http://localhost:8081/proizvodi/1')
+    print('proizvod_1_res: ', await proizvod_1_res.text())
+    proizvod_7_res = await session.get('http://localhost:8081/proizvodi/7')
+    print('proizvod_1_res: ', await proizvod_7_res.text())
 
 asyncio.run(main())
